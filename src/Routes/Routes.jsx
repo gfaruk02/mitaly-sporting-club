8c4f468dfe5140dceb5298f64ee232d52480ea03
gfaruk02 <gfaruk02@gmail.com>
2023-12-28 22:48:55 +0600

    create contact page

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,6 +3,9 @@ import {
   } from "react-router-dom";
 import Main from "../Layout/Main/Main";
 import Home from "../Pages/Home/Home";
+import Contact from "../Pages/Contact/Contact";
+import Register from "../Pages/Register/Register";
+import Login from "../Pages/Login/Login";
   
   export const router = createBrowserRouter([
     {
@@ -12,6 +15,18 @@ import Home from "../Pages/Home/Home";
         {
             path: "/",
             element:<Home></Home>
+        },
+        {
+          path:'/contact',
+          element: <Contact></Contact>
+        },
+        {
+          path: '/register',
+          element: <Register></Register>
+        },
+        {
+          path: '/login',
+          element: <Login></Login>
         }
       ]
     },
